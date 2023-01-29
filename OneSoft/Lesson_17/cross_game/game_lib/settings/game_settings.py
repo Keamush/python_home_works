3766c056711da291d2360a9389d7d9ec4b998124
keamush <09kim.elena@gmail.com>
2023-01-29 17:48:15 +0600

    1

--- OneSoft/Lesson_17/cross_game/game_lib/settings/game_settings.py
+++ OneSoft/Lesson_17/cross_game/game_lib/settings/game_settings.py
@@ -1,3 +1,5 @@
+import pygame
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 PLAYER_START_POSITION = (0, -SCREEN_HEIGHT // 2 + 20)
@@ -6,3 +8,5 @@ CAR_COLORS = ('red', 'orange', 'yellow', 'green', 'blue', 'purple')
 PLAYER_MOVE_DISTANCE = 10
 FINISH_LINE = SCREEN_HEIGHT // 2 - 20
 FONT = ('Arial', 20, 'bold')
+
+background_image = pygame.image.load()
